6c104178af921a117d4c83538cd6dfceaf4acc9b
abulbashar98 <abulbashar98116@gmail.com>
2022-03-10 14:33:52 +0600

    ternary-operator

--- shortcut.js
+++ shortcut.js
@@ -47,3 +47,50 @@ console.log(food1);
 // Multiple Conditions 
 const food2 = (money < 200 && money > 100) ? 'kachchi' : 'nan - grill';
 console.log(food2);
+
+
+
+
+/*-------------- *\
+   Shortcuts
+\*-----------------*/
+
+
+// convert to string........
+
+let amount = 5000;
+amountStr = amount + '';
+console.log(amountStr);
+
+
+
+// Convert to number.....
+
+const amountText = '7890021';
+const amountNum = +amountText;
+console.log(amountNum);
+
+
+
+
+// short cut uses of && and || when we dont need a 
+// return lke we get after ternary operator ........
+
+let isActive = false;
+
+const showUser = () => {
+    console.log('User shown')
+}
+
+const hideUser = () => {
+    console.log('User hidden')
+}
+
+// 1. if variable is true function execution is true..... 
+
+isActive && showUser();
+
+
+// 2. if  variable is false function execution is true.....
+//    if  variable is true function execution is false.....
+isActive || hideUser();
